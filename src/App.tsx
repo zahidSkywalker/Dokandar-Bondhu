ab93422ec00731d5e0a83744d3bebd588ce9ae75
Jahidul Islam <ionzahid0987@gmail.com>
2026-02-03 11:40:01 +0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AppProvider } from './context/AppContext';
+import { LanguageProvider } from './context/LanguageContext';
+import MainLayout from './components/layout/MainLayout'; // We will create this in Phase 3
+
+const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <LanguageProvider>
+        <AppProvider>
+          <MainLayout />
+        </AppProvider>
+      </LanguageProvider>
+    </React.StrictMode>
+  );
+};
+
+export default App;
