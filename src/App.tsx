c769c37d488be11a1d0e185aa15177b256167952
Jahidul Islam <ionzahid0987@gmail.com>
2026-02-12 15:42:41 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { AppProvider } from './context/AppContext';
 import { LanguageProvider } from './context/LanguageContext';
-import { ThemeProvider } from './context/ThemeContext'; // <--- IMPORT THIS
+import { ThemeProvider } from './context/ThemeContext';
+import { SettingsProvider } from './context/SettingsContext'; // NEW
 import MainLayout from './components/layout/MainLayout';
 
 const App: React.FC = () => {
   return (
     <React.StrictMode>
       <LanguageProvider>
-        <ThemeProvider> {/* <--- WRAP EVERYTHING IN THIS */}
-          <AppProvider>
-            <MainLayout />
-          </AppProvider>
+        <ThemeProvider>
+          <SettingsProvider> {/* NEW */}
+            <AppProvider>
+              <MainLayout />
+            </AppProvider>
+          </SettingsProvider>
         </ThemeProvider>
       </LanguageProvider>
     </React.StrictMode>
