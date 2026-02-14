e7cd90d46528a2fda4a35e8adca76c6d4b32a09a
Jahidul Islam <ionzahid0987@gmail.com>
2026-02-14 20:04:19 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// Import the License Guard
+import { checkLicenseAndAccess } from './lib/license';
+
+// 1. Run the License Check BEFORE rendering React
+const isAccessAllowed = checkLicenseAndAccess();
+
+// 2. Only render the App if License is Valid or Trial is Active
+if (isAccessAllowed) {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
